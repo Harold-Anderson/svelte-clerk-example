003f7c4726bfaa93ff181885a9eb16606b9bd826
Harold Anderson <harold@acfo.co>
2024-10-21 18:00:57 -0700

    foo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { LayoutData } from './$types';
+	import { ClerkProvider,SignIn } from 'svelte-clerk';
+	import { PUBLIC_CLERK_PUBLISHABLE_KEY } from '$env/static/public';
+
+	const {
+		children,
+		data
+	}: {
+		children: Snippet;
+		data: LayoutData;
+	} = $props();
+</script>
+
+<!-- ... -->
+
+<ClerkProvider {...data} publishableKey={PUBLIC_CLERK_PUBLISHABLE_KEY}>
+	<SignIn /> 
+</ClerkProvider>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
