b7ea34051f6c925bdbbbed22f719c4cbb72fb4ba
Robert Soriano <sorianorobertc@gmail.com>
2024-10-23 07:48:01 -0700

    Update +page.server.ts

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -3,7 +3,7 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	if (!locals.auth.userId) {
-	  return redirect(301, '/sign-in')
+	  return redirect(307, '/sign-in')
 	}
 
 	return {};
